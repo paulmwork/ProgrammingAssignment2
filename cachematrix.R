cf31c9ded01963a54feb4a4259d2edcab8dd31cc
paulmwork <paulmwork@gmail.com>
2014-06-17 08:58:55 -0700

    Updated makeCacheMatrix and cacheSolve Functions
    Implemented several clean-up changes.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix and cacheSolve functions work together to make a matrix inversion
+# calculation run faster and with fewer computational resources if the inverse has already
+# been calculated and the matrix has not changed.  It does this by using a cached value of the 
+# matrix inversion calculation when the above conditions are met.
 
-## Write a short comment describing this function
+
+
+## The makeCacheMatrix function uses linear scoping to creat a matrix object that can cache
+# a value for a matrix inversion.
 
 makeCacheMatrix <- function(x = matrix()) {
         
@@ -22,10 +27,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function calculates the matrix inversion of the above-matrix and tests
+# to see if the matrix inversion is unchanged and, if so, returns the previously calculated and 
+# cached value thereof.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
             
                 m <- x$cacheSolve()
                 if(!is.null(m)) {
